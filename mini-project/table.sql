5786155e597414889446342773a988f226455f52
avi-by <45941093+avi-by@users.noreply.github.com>
2019-05-05 18:16:54 +0300

    Update table.sql

--- mini-project/table.sql
+++ mini-project/table.sql
@@ -1,34 +1,46 @@
 CREATE TABLE customer
 (
   customerId INT NOT NULL,
-  Subscription_Type INT NOT NULL,
+  customer_Membership INT NOT NULL,
   registration_date INT NOT NULL,
   phone_number INT,
   email INT NOT NULL,
   PRIMARY KEY (customerId)
 );
 
-CREATE TABLE order
+CREATE TABLE orders
 (
   orderId INT NOT NULL,
-  date INT NOT NULL,
-  discount INT,
+  orderDate INT NOT NULL,
+  orderDiscount INT,
   customerId INT NOT NULL,
   PRIMARY KEY (orderId),
   FOREIGN KEY (customerId) REFERENCES customer(customerId)
 );
 
-CREATE TABLE ticket
+CREATE TABLE movies
 (
-  line INT NOT NULL,
-  ticketId INT NOT NULL,
-  status INT NOT NULL,
+  movieID INT NOT NULL,
+  removalDate INT NOT NULL,
   movieName INT NOT NULL,
-  movieId INT NOT NULL,
-  chair INT NOT NULL,
+  genre INT NOT NULL,
+  rating INT NOT NULL,
+  releaseDate INT NOT NULL,
+  releaseYear INT NOT NULL,
+  PRIMARY KEY (movieID)
+);
+
+CREATE TABLE tickets
+(
+  ticketLine INT NOT NULL,
+  ticketId INT NOT NULL,
+  ticketStatus INT NOT NULL,
+  movieName INT NOT NULL,
+  ticketChair INT NOT NULL,
   theaterId INT NOT NULL,
   orderId INT NOT NULL,
+  movieID INT NOT NULL,
   PRIMARY KEY (ticketId),
-  FOREIGN KEY (orderId) REFERENCES order(orderId),
-  UNIQUE (movieId)
+  FOREIGN KEY (orderId) REFERENCES orders(orderId),
+  FOREIGN KEY (movieID) REFERENCES movies(movieID)
 );
